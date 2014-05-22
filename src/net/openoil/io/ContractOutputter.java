4c119915d724f7e76769bac5b3daea87f73ca2a9
Karl Beecher <karl@endocode.com>
2014-05-22 18:01:45 +0200

    Adds capability to output to file
    If output to file fails, reverts to console output.
    
    Change-Id: I2f37628d1b2a376979d14fd3cc77dc896839f713
    Ref: #946

--- src/net/openoil/io/ContractOutputter.java
+++ src/net/openoil/io/ContractOutputter.java
@@ -1,5 +1,8 @@
 package net.openoil.io;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Iterator;
 import java.util.List;
 
@@ -10,38 +13,92 @@ public class ContractOutputter {
 
     private OutputTable outputs;
 
+    private StringBuilder outputStr;
+    private BufferedWriter fileWriter;
+
+    private boolean outputToFile = true;
+
     public ContractOutputter(OutputTable outputs) {
         this.outputs = outputs;
     }
 
     public void output() {
-        StringBuilder outputStr = new StringBuilder();
+        outputStr = new StringBuilder();
 
-        addToOutput(outputStr, "Year", outputs.getYear());
-        addToOutput(outputStr, "Production", outputs.getProduction());
-        addToOutput(outputStr, "Price", outputs.getPrice());
-        addToOutput(outputStr, "Surface Rental", outputs.getSurfaceRental());
-        addToOutput(outputStr, "Flat Royalty", outputs.getFlatRoyalty());
-        addToOutput(outputStr, "Capex", outputs.getCapex());
-        addToOutput(outputStr, "Opex", outputs.getOpex());
-        addToOutput(outputStr, "Daily Production Royalty",
+        try {
+            fileWriter = new BufferedWriter(new FileWriter("output.csv"));
+        } catch (IOException e) {
+            // TODO Proper error handling
+            System.out.println("Cannot write to file: " + e.getMessage());
+            System.out.println("Outputting to the console instead");
+            outputToFile = false;
+        }
+
+        addToOutput("Year", outputs.getYear());
+        addToOutput("Production", outputs.getProduction());
+        addToOutput("Price", outputs.getPrice());
+        addToOutput("Surface Rental", outputs.getSurfaceRental());
+        addToOutput("Flat Royalty", outputs.getFlatRoyalty());
+        addToOutput("Capex", outputs.getCapex());
+        addToOutput("Opex", outputs.getOpex());
+        addToOutput("Daily Production Royalty",
                 outputs.getDailyProductionRoyalty());
-        addToOutput(outputStr, "Cumulative Production Royalty",
+        addToOutput("Cumulative Production Royalty",
                 outputs.getCumulativeProductionRoyalty());
-        addToOutput(outputStr, "Cumulative Recoverable Cost",
+        addToOutput("Cumulative Recoverable Cost",
                 outputs.getCumulativeRecoverableCosts());
-        addToOutput(outputStr, "Recovered Costs", outputs.getCostRecovery());
-        addToOutput(outputStr, "Profit Oil", outputs.getProfitOil());
-        addToOutput(outputStr, "Government Share", outputs.getGovernmentShare());
-        addToOutput(outputStr, "Company Share", outputs.getCompanyShare());
-        addToOutput(outputStr, "Corporate Income Tax",
-                outputs.getCorporateIncomeTax());
+        addToOutput("Recovered Costs", outputs.getCostRecovery());
+        addToOutput("Profit Oil", outputs.getProfitOil());
+        addToOutput("Government Share", outputs.getGovernmentShare());
+        addToOutput("Company Share", outputs.getCompanyShare());
+        addToOutput("Corporate Income Tax", outputs.getCorporateIncomeTax());
 
-        System.out.println(outputStr);
+        if (outputToFile) {
+            try {
+                fileWriter.close();
+            } catch (IOException e) {
+                System.out.println("Error writing to file: " + e.getMessage());
+            }
+        } else {
+            System.out.println(outputStr);
+        }
     }
 
-    private void addToOutput(StringBuilder outputStr, String rowName,
-            List<?> figures) {
+    private void addToOutput(String rowName, List<?> figures) {
+        if (outputToFile) {
+            addToFile(rowName, figures);
+        } else {
+            addToString(rowName, figures);
+        }
+    }
+
+    private void addToFile(String rowName, List<?> figures) {
+        if (null == figures) {
+            return;
+        }
+
+        Iterator<?> i = figures.iterator();
+
+        try {
+            fileWriter.append(rowName + ",");
+
+            while (i.hasNext()) {
+                Object figure = i.next();
+                fileWriter.append(figure.toString());
+
+                if (i.hasNext()) {
+                    fileWriter.append(",");
+                }
+            }
+            fileWriter.append("\n");
+        } catch (IOException e) {
+            System.out.println("Cannot write to file: " + e.getMessage());
+            return;
+        }
+
+    }
+
+    private void addToString(String rowName, List<?> figures) {
 
         if (null == figures) {
             return;
