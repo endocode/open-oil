10e1b301c88355a5695300c5c8209ea90e7d318c
Karl Beecher <karl@endocode.com>
2014-05-23 12:46:42 +0200

    Added ability to output to an ODS file
    The output is now written to an ODS file. If there is a problem writing
    the file, the output goes to stdout instead.
    
    Change-Id: Ifefe0b0bcb801723f5c061c5efb9ef73fed6232b
    Ref: #946

--- src/net/openoil/io/ContractOutputter.java
+++ src/net/openoil/io/ContractOutputter.java
@@ -1,10 +1,15 @@
 package net.openoil.io;
 
 import java.io.BufferedWriter;
-import java.io.FileWriter;
+import java.io.File;
 import java.io.IOException;
 import java.util.Iterator;
 import java.util.List;
+
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableModel;
+
+import org.jopendocument.dom.spreadsheet.SpreadSheet;
 
 /**
  * This class formats the data in the OutputTable into a suitable form.
@@ -14,95 +19,112 @@ public class ContractOutputter {
     private OutputTable outputs;
 
     private StringBuilder outputStr;
+
     private BufferedWriter fileWriter;
+    private Object[][] odsData;
+    private String[] columnNames;
+
+    private int rowNum = 0;
+    // TODO Work out number of rows!
+    final int ROWS = 15;
+    int COLUMNS;
 
     private boolean outputToFile = true;
 
+    // TODO Put in resource files
+    private final String SURFACE_RENTAL_DESC = "Acreage (N ha) x Rental per Km ($ N / Km).";
+    private final String FLAT_ROYALTY_DESC = "Flat royalty rate (N %) x Gross sales ($ Nmm).";
+    private final String OPEX_DESC = "Production (N mmbbls) multiplied by opex per barrel ($ N)";
+    private final String DAILY_PROD_ROYALTY_DESC = "Sum of tranche amounts. Tranche 1 ($ Nmm) + Tranche 2 ($ Nmm) + Tranche 3 ($ Nmm) + Tranche 4 ($ Nmm)";
+    private final String CUM_PROD_ROYALTY_DESC = "Sum of tranche amounts. Tranche 1 ($ Nmm) + Tranche 2 ($ Nmm) + Tranche 3 ($ Nmm) + Tranche 4 ($ Nmm)";
+    private final String PROFIT_OIL_DESC = "Gross sales ($ Nmm) - Total royalty ($ Nmm) - Cost recovery ($ Nmm)";
+    private final String CIT_DESC = "(Total profit oil ($ Nmm) - Government share ($ Nmm) ) x Corporate income tax rate (N %)";
+
     public ContractOutputter(OutputTable outputs) {
         this.outputs = outputs;
+        COLUMNS = outputs.getYear().size() + 2;
+
+        odsData = new Object[ROWS][COLUMNS + 1];
     }
 
     public void output() {
         outputStr = new StringBuilder();
 
-        try {
-            fileWriter = new BufferedWriter(new FileWriter("output.csv"));
-        } catch (IOException e) {
-            // TODO Proper error handling
-            System.out.println("Cannot write to file: " + e.getMessage());
-            System.out.println("Outputting to the console instead");
-            outputToFile = false;
-        }
-
         addToOutput("Year", outputs.getYear());
         addToOutput("Production", outputs.getProduction());
         addToOutput("Price", outputs.getPrice());
-        addToOutput("Surface Rental", outputs.getSurfaceRental());
-        addToOutput("Flat Royalty", outputs.getFlatRoyalty());
+        addToOutput("Surface Rental", outputs.getSurfaceRental(),
+                SURFACE_RENTAL_DESC);
+        addToOutput("Flat Royalty", outputs.getFlatRoyalty(), FLAT_ROYALTY_DESC);
         addToOutput("Capex", outputs.getCapex());
-        addToOutput("Opex", outputs.getOpex());
+        addToOutput("Opex", outputs.getOpex(), OPEX_DESC);
         addToOutput("Daily Production Royalty",
-                outputs.getDailyProductionRoyalty());
+                outputs.getDailyProductionRoyalty(), DAILY_PROD_ROYALTY_DESC);
         addToOutput("Cumulative Production Royalty",
-                outputs.getCumulativeProductionRoyalty());
+                outputs.getCumulativeProductionRoyalty(), CUM_PROD_ROYALTY_DESC);
         addToOutput("Cumulative Recoverable Cost",
                 outputs.getCumulativeRecoverableCosts());
         addToOutput("Recovered Costs", outputs.getCostRecovery());
-        addToOutput("Profit Oil", outputs.getProfitOil());
+        addToOutput("Profit Oil", outputs.getProfitOil(), PROFIT_OIL_DESC);
         addToOutput("Government Share", outputs.getGovernmentShare());
         addToOutput("Company Share", outputs.getCompanyShare());
-        addToOutput("Corporate Income Tax", outputs.getCorporateIncomeTax());
+        addToOutput("Corporate Income Tax", outputs.getCorporateIncomeTax(),
+                CIT_DESC);
 
-        if (outputToFile) {
-            try {
-                fileWriter.close();
-            } catch (IOException e) {
-                System.out.println("Error writing to file: " + e.getMessage());
+        try {
+            columnNames = new String[COLUMNS];
+            // TODO Create a spreadsheet without column names
+            for (int y = 1; y <= outputs.getYear().size(); y++) {
+                columnNames[y] = "";
             }
-        } else {
+
+            File f = new File("output.ods");
+            TableModel model = new DefaultTableModel(odsData, columnNames);
+            SpreadSheet.createEmpty(model).saveAs(f);
+
+            SpreadSheet s = SpreadSheet.createEmpty(model);
+
+        } catch (IOException e) {
+            System.out.println("Unable to create file: " + e.getMessage());
+            System.out.println("Printing to console instead...");
+            outputToFile = false;
+        }
+
+        if (!outputToFile) {
             System.out.println(outputStr);
         }
     }
 
     private void addToOutput(String rowName, List<?> figures) {
-        if (outputToFile) {
-            addToFile(rowName, figures);
-        } else {
-            addToString(rowName, figures);
-        }
+        addToOutput(rowName, figures, "");
     }
 
-    private void addToFile(String rowName, List<?> figures) {
+    private void addToOutput(String rowName, List<?> figures,
+            String rowDescription) {
         if (null == figures) {
             return;
         }
 
-        Iterator<?> i = figures.iterator();
+        addToFile(rowName, figures, rowDescription);
+        addToString(rowName, figures);
+    }
 
-        try {
-            fileWriter.append(rowName + ",");
+    private void addToFile(String rowName, List<?> figures,
+            String rowDescription) {
 
-            while (i.hasNext()) {
-                Object figure = i.next();
-                fileWriter.append(figure.toString());
+        Object[] newRow = new Object[COLUMNS + 1];
+        newRow[0] = rowName;
+        newRow[1] = rowDescription;
 
-                if (i.hasNext()) {
-                    fileWriter.append(",");
-                }
-            }
-            fileWriter.append("\n");
-        } catch (IOException e) {
-            System.out.println("Cannot write to file: " + e.getMessage());
-            return;
+        for (int i = 0; i < figures.size(); i++) {
+            newRow[i + 2] = figures.get(i);
         }
 
+        odsData[rowNum] = newRow;
+        rowNum++;
     }
 
     private void addToString(String rowName, List<?> figures) {
-
-        if (null == figures) {
-            return;
-        }
 
         Iterator<?> i = figures.iterator();
 
