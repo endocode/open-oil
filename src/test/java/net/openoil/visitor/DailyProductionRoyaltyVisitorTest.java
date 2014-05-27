b9a80e51bce1f518260d801906be6e893fe4fd10
Krzesimir Nowak <krzesimir@endocode.com>
2014-05-27 12:14:50 +0200

    Add test for DailyProductionRoyaltyVisitor.
    Refs #929
    
    Change-Id: I051fe02d913d3ed508120b87c8afa3409858fb4b

---
+++ src/test/java/net/openoil/visitor/DailyProductionRoyaltyVisitorTest.java
@@ -0,0 +1,27 @@
+package net.openoil.visitor;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+import net.openoil.element.DailyProductionRoyaltyElement;
+
+import net.openoil.TestUtil;
+
+@RunWith(JUnit4.class)
+public class DailyProductionRoyaltyVisitorTest {
+
+    @Test
+    public void royaltyEquality() {
+        DailyProductionRoyaltyElement royalty = Harness.getDailyProductionRoyaltyElement();
+        DailyProductionRoyaltyVisitor visitor = new DailyProductionRoyaltyVisitor();
+
+        visitor.visit(Harness.getFilledYearElement());
+        visitor.visit(Harness.getFilledPriceElement());
+        visitor.visit(Harness.getFilledProductionElement());
+        visitor.visit(royalty);
+
+        TestUtil.assertEquals(Harness.getDailyProductionRoyalty(), royalty.getDailyProductionRoyalty());
+    }
+
+}
--- src/test/java/net/openoil/visitor/Harness.java
+++ src/test/java/net/openoil/visitor/Harness.java
@@ -6,11 +6,13 @@ import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 
+import net.openoil.element.DailyProductionRoyaltyElement;
 import net.openoil.element.FlatRoyaltyElement;
 import net.openoil.element.OpexElement;
 import net.openoil.element.PriceElement;
 import net.openoil.element.ProductionElement;
 import net.openoil.element.SurfaceRentalElement;
+import net.openoil.element.YearElement;
 
 public class Harness {
 
@@ -142,6 +144,51 @@ public class Harness {
         return opex;
     }
 
+    // DAILY PRODUCTION ROYALTY
+
+    public static DailyProductionRoyaltyElement getFilledDailyProductionRoyaltyElement() {
+        DailyProductionRoyaltyElement element = getDailyProductionRoyaltyElement();
+
+        element.setDailyProductionRoyalty(getDailyProductionRoyalty());
+
+        return element;
+    }
+
+    public static DailyProductionRoyaltyElement getDailyProductionRoyaltyElement() {
+        DailyProductionRoyaltyElement element = new DailyProductionRoyaltyElement();
+
+        setDailyTranches(element, getDailyTranches());
+
+        return element;
+    }
+
+    public static List<BigDecimal> getDailyProductionRoyalty() {
+        List<BigDecimal> daily = new ArrayList<BigDecimal>(YEAR_COUNT);
+
+        daily.add(new BigDecimal(0));
+        daily.add(new BigDecimal(0));
+        daily.add(new BigDecimal(0));
+        daily.add(new BigDecimal(0));
+        daily.add(new BigDecimal("6.51"));
+        daily.add(new BigDecimal("133.18"));
+        daily.add(new BigDecimal("119.95"));
+        daily.add(new BigDecimal("117.99"));
+
+        assertEquals(YEAR_COUNT, daily.size());
+
+        return daily;
+    }
+
+    // YEAR
+
+    public static YearElement getFilledYearElement() {
+        YearElement element = new YearElement();
+
+        element.setYear(getYear());
+
+        return element;
+    }
+
     // SURFACE RENTAL PRIVATE
 
     private static List<BigDecimal> getRentalPerKm() {
@@ -238,6 +285,56 @@ public class Harness {
         assertEquals(1, opexPerBarrel.size());
 
         return opexPerBarrel;
+    }
+
+    // DAILY PRODUCTION ROYALTY PRIVATE
+
+    private static void setDailyTranches(DailyProductionRoyaltyElement royalty, List<List<BigDecimal>> tranches) {
+        for (List<BigDecimal> tranche : tranches) {
+            royalty.addTranche(tranche);
+        }
+    }
+
+    private static List<List<BigDecimal>> getDailyTranches() {
+        final int trancheCount = 4;
+        List<List<BigDecimal>> tranches = new ArrayList<List<BigDecimal>>(trancheCount);
+
+        tranches.add(createDailyTranche(0, 2.5, 5));
+        tranches.add(createDailyTranche(2.5, 5, 7.5));
+        tranches.add(createDailyTranche(5, 7.5, 10));
+        tranches.add(createDailyTranche(7.5, 1000000, 12.5));
+
+        assertEquals(trancheCount, tranches.size());
+
+        return tranches;
+    }
+
+    private static List<BigDecimal> createDailyTranche(double lmbopd, double umbopd, double rate)
+    {
+        final int valueCount = 3;
+        List<BigDecimal> tranche = new ArrayList<BigDecimal>(valueCount);
+
+        tranche.add(new BigDecimal(lmbopd));
+        tranche.add(new BigDecimal(umbopd));
+        tranche.add(new BigDecimal(rate));
+
+        assertEquals(valueCount, tranche.size());
+
+        return tranche;
+    }
+
+    // YEAR PRIVATE
+
+    private static List<Integer> getYear() {
+        List<Integer> years = new ArrayList<Integer>(YEAR_COUNT);
+
+        for (int i = 1; i <= YEAR_COUNT; ++i) {
+            years.add(i);
+        }
+
+        assertEquals(YEAR_COUNT, years.size());
+
+        return years;
     }
 
 }
