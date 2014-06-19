e420666407ddad7278c195d411ac3aa715cee73b
Karl Beecher <karl@endocode.com>
2014-06-19 23:14:15 +0200

    Fixes some problems in R-factpr algorithm.
    Outputs now match Afghanistan example.
    
    Change-Id: I9f45e0618da1de08099e310b738cac6e62ec7f9c

--- src/main/java/net/openoil/visitor/ProductionSharingRFactorVisitor.java
+++ src/main/java/net/openoil/visitor/ProductionSharingRFactorVisitor.java
@@ -68,71 +68,79 @@ public class ProductionSharingRFactorVisitor extends DefaultVisitor {
         // rFactor is automatically 0 in the first year.
         rFactor.add(0d);
 
-        BigDecimal cumulativeCapex = BigDecimal.ZERO;
-        BigDecimal cumulativeOpex = BigDecimal.ZERO;
         double rFactorThisYear = 0d;
 
         BigDecimal profitOilThisYear;
+        BigDecimal companySharePercentage = BigDecimal.ZERO;
 
         List<BigDecimal> governmentShare = new ArrayList<BigDecimal>();
         List<BigDecimal> companyShare = new ArrayList<BigDecimal>();
+        List<BigDecimal> X = new ArrayList<BigDecimal>();
+        List<BigDecimal> Y = new ArrayList<BigDecimal>();
 
         for (int y = 0; y < year.size(); y++) {
 
-            cumulativeCapex = cumulativeCapex.add(capex.get(y));
-            cumulativeOpex = cumulativeOpex.add(opex.get(y));
             profitOilThisYear = profitOil.get(y);
 
-            // If there's no profit oil to split, next year's R-Factor is 0.
+            // If there's no profit oil to split, shares are 0.
             if (profitOilThisYear.compareTo(BigDecimal.ZERO) == 0) {
-                rFactor.add(0d);
                 governmentShare.add(BigDecimal.ZERO);
                 companyShare.add(BigDecimal.ZERO);
-                continue;
+            } else {
+
+                rFactorThisYear = rFactor.get(y);
+
+                // Determine government's share (P) according to the R-Factor
+                // table
+                // TODO Some way for user to enter this is needed; for now the
+                // table is hard-coded
+                // when R <= 1, P = 0
+                // when 1 < R <= 1.3, P = 0.5
+                // when 1.3 < R <= 2.5, P = 0.7 - [ (2.5 - R) / (2.5-1.3) ] *
+                // 0.2
+                // when R > 2.5, P = 0.7
+                double P = 0d;
+                if (rFactorThisYear <= 1) {
+                    P = 0d;
+                } else if (rFactorThisYear > 1 && rFactorThisYear <= 1.3) {
+                    P = 0.5d;
+                } else if (rFactorThisYear > 1.3 && rFactorThisYear <= 2.5) {
+                    P = 0.7d - ((2.5d - rFactorThisYear) / 2.5d - 1.3d) * 0.2d;
+                } else if (rFactorThisYear > 2.5) {
+                    P = 0.7d;
+                }
+                companySharePercentage = new BigDecimal(1 - P);
+
+                BigDecimal governmentProfit = profitOilThisYear
+                        .multiply(new BigDecimal(P));
+                BigDecimal companyProfit = profitOilThisYear
+                        .multiply(companySharePercentage);
+
+                governmentShare.add(governmentProfit.setScale(2,
+                        RoundingMode.UP));
+                companyShare.add(companyProfit.setScale(2, RoundingMode.UP));
             }
-
-            rFactorThisYear = rFactor.get(y);
-
-            // Determine government's share (P) according to the R-Factor table
-            // TODO Some way for user to enter this is needed; for now the table
-            // is hard-coded
-            // when R <= 1, P = 0
-            // when 1 < R <= 1.3, P = 0.5
-            // when 1.3 < R <= 2.5, P = 0.7 - [ (2.5 - R) / (2.5-1.3) ] * 0.2
-            // when R > 2.5, P = 0.7
-            double P = 0;
-            if (rFactorThisYear <= 1) {
-                P = 0d;
-            } else if (rFactorThisYear > 1 && rFactorThisYear <= 1.3) {
-                P = 0d;
-            } else if (rFactorThisYear > 1.3 && rFactorThisYear < 2.5) {
-                P = 0.7d - ((2.5d - rFactorThisYear) / 2.5d - 1.3d) * 0.2d;
-            } else if (rFactorThisYear > 2.5) {
-                P = 0.7d;
-            }
-
-            BigDecimal governmentProfit = profitOilThisYear
-                    .multiply(new BigDecimal(P));
-            BigDecimal companyProfit = profitOilThisYear
-                    .multiply(new BigDecimal(1 - P));
-
-            governmentShare.add(governmentProfit.setScale(2, RoundingMode.UP));
-            companyShare.add(companyProfit.setScale(2, RoundingMode.UP));
 
             // Determine next year's R-Factor
             // R_n+1 = X / Y, where:
             // X = RecoveredCost_n + companyShareProfitOil_n
             // Y = CumulativeOpex_n + CumulativeCapex_n
+            BigDecimal xLastYear = y > 0 ? X.get(y - 1) : BigDecimal.ZERO;
+            BigDecimal yLastYear = y > 0 ? Y.get(y - 1) : BigDecimal.ZERO;
+
             BigDecimal companyShareProfitOil = profitOilThisYear
-                    .multiply(new BigDecimal(rFactor.get(y)));
-            BigDecimal X = recoveredCosts.get(y).add(companyShareProfitOil);
-            BigDecimal Y = cumulativeCapex.add(cumulativeOpex);
-            rFactor.add(X.divide(Y, new MathContext(2, RoundingMode.DOWN))
-                    .doubleValue());
+                    .multiply(companySharePercentage);
+            BigDecimal xThisYear = xLastYear.add(recoveredCosts.get(y)).add(
+                    companyShareProfitOil);
+            BigDecimal yThisYear = yLastYear.add(capex.get(y)).add(opex.get(y));
+
+            rFactor.add(xThisYear.divide(yThisYear,
+                    new MathContext(3, RoundingMode.DOWN)).doubleValue());
+            X.add(xThisYear);
+            Y.add(yThisYear);
         }
 
         productionSharingRFactorElement.setGovernmentShare(governmentShare);
         productionSharingRFactorElement.setCompanyShare(companyShare);
     }
-
 }
